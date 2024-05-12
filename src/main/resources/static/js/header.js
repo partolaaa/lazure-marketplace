42a8651ada0c0096196d065fd148b0200004c1a8
partolaaa <ivan.partola77@gmail.com>
2024-05-12 04:00:08 +0200

    Add colorful background animation to marketplace page

--- src/main/resources/static/js/header.js
+++ src/main/resources/static/js/header.js
@@ -39,8 +39,36 @@ document.getElementById('menu').addEventListener('click', function(event) {
     event.stopPropagation();
 });
 
-/*document.getElementById('profile-balance').addEventListener('click', function(event) {
-    document.getElementById('wallet-info').classList.add('open-wallet-info');
-    event.stopPropagation();
-});*/
+document.addEventListener("DOMContentLoaded", function() {
+    const background = document.getElementById('background');
+    let colorArr = ["rgba(255, 97, 179, 0.5)", "rgba(255, 97, 133, 0.5)", "rgba(78, 184, 251, 0.5)"];
+
+    function randomRange(min, max) {
+        return Math.random() * (max - min) + min;
+    }
+
+    for (let i = 0; i < 5; i++) {
+        let circle = document.createElement('div');
+        circle.classList.add('circle');
+        circle.style.width = circle.style.height = `${randomRange(50, 150)}px`;
+        circle.style.position = 'absolute';
+        circle.style.left = `${randomRange(0, 100)}vw`;
+        circle.style.top = `${randomRange(0, 100)}vh`;
+        circle.style.backgroundColor = colorArr[Math.floor(Math.random() * colorArr.length)];
+        background.appendChild(circle);
+
+        function moveCircle() {
+            const newX = randomRange(0, 100);
+            const newY = randomRange(0, 100);
+            circle.style.transition = `all ${randomRange(20, 40)}s ease`;
+            circle.style.left = `${newX}vw`;
+            circle.style.top = `${newY}vh`;
+        }
+
+        setInterval(moveCircle, randomRange(20000, 40000));
+    }
+});
+
+
+
 

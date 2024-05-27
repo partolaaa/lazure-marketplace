488120186979f5c575ab40eed78f5adbc68bbc41
partolaaa <ivan.partola77@gmail.com>
2024-05-27 14:19:40 +0200

    Add Jetty configuration

--- src/main/java/com/lazure/LazureApplication.java
+++ src/main/java/com/lazure/LazureApplication.java
@@ -2,12 +2,20 @@ package com.lazure;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class LazureApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(LazureApplication.class, args);
+    }
+
+    @Bean
+    public ServletWebServerFactory servletWebServerFactory() {
+        return new JettyServletWebServerFactory();
     }
 
 }
